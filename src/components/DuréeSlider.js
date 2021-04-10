af3ca150ef57e7914444fbb442fa2c841437f7bd
Maya Ausset <mayaausset@gmail.com>
2021-04-10 19:26:31 +0200

    duree slider

--- src/components/DuréeSlider.js
+++ src/components/DuréeSlider.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Slider } from "@material-ui/core";
 
 const DuréeSlider = () => {
-    return (
-        <div>
-            <h1>Durée</h1>
+  const [durée, setdurée] = useState(2, 12);
+
+  const updatedurée = (e, data) => {
+    console.log(durée);
+    setdurée(data);
+  };
+  return (
+    <div>
+      <div className="slider">
+        <h4>Je souhaite emprunter sur une durée de {durée} mois</h4>
+        <div className="slider-bar">
+          <p>2 mois</p>
+          <Slider
+            min={2}
+            max={12}
+            step={1}
+            value={durée}
+            onChange={updatedurée}
+          />
+          <p>12 mois</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default DuréeSlider;
--- src/components/PrixSlider.js
+++ src/components/PrixSlider.js
@@ -12,7 +12,7 @@ const PrixSlider = () => {
         <div className="slider">
             <h4>Je souhaite emprunter {prix} euros</h4>
             <div className="slider-bar">
-            <p>1000€</p>
+            <p>1000 €</p>
             <Slider
             min={1000}
             max={10000}
@@ -20,7 +20,7 @@ const PrixSlider = () => {
             value={prix}
             onChange={updatePrix}
             />
-            <p>10 000€</p>
+            <p>10 000 €</p>
             </div>
         </div>
     );
